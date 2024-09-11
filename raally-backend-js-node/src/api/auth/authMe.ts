387e263895f1b7905dba2d5b3bda2c5520529f28
mahaboob-basha-a <alurumahaboobbasha@gmail.com>
2024-09-11 16:32:16 +0530

    first

---
+++ raally-backend-js-node/src/api/auth/authMe.ts
@@ -0,0 +1,17 @@
+import ApiResponseHandler from '../apiResponseHandler';
+import Error403 from '../../errors/Error403';
+
+export default async (req, res, next) => {
+  try {
+    if (!req.currentUser || !req.currentUser.id) {
+      throw new Error403(req.language);
+    }
+
+    const payload = req.currentUser;
+    next()
+
+    await ApiResponseHandler.success(req, res, payload);
+  } catch (error) {
+    await ApiResponseHandler.error(req, res, error);
+  }
+};
